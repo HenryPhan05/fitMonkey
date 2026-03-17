d25b7c3c84e6f97f12ba24fd630b0f0cf27a8af6
gjasmeen <jasmeen.garcha@edu.sait.ca>
2026-03-16 21:02:25 -0600

    center

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,8 @@ import Link from 'next/link';
 
 export default function Home() {
   return (
-    <main>
-      <div className='bg-blue-900 text-center mt-10 rounded-2xl' >
+    <main className='justify-content-center items-center flex flex-col'>
+      <div className='bg-blue-900 text-center mt-10 rounded-2xl w-100' >
         <h1 className='text-2xl font-bold text-white'>Fit Monkey 🐒</h1>
         <p className='text-white'>Be fit like a Monkey!</p>
         <p className='text-white'>Track your fitness journey with Fit Monkey!</p>
