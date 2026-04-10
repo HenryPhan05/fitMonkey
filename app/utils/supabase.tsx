d3cefc838d94b5975f0a0cd20530cdab861329e9
Mariel Bananola <theplanetm4rs@gmail.com>
2026-04-10 16:41:58 -0600

    Edited Supabase API Keys
    small changes to the supabse api keys, needs more debugging

--- app/utils/supabase.tsx
+++ app/utils/supabase.tsx
@@ -1,7 +1,7 @@
 // utils/supabase.ts
-import { createClient } from '@supabase/supabase-js'
+import { createBrowserClient } from '@supabase/ssr'
 
-const SUPABASE_URL = 'https://uhrjptuvkvajpqznyikd.supabase.co'
-const SUPABASE_ANON_KEY = 'https://uhrjptuvkvajpqznyikd.supabase.co'
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
+export const supabase = createBrowserClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_DEFAULT_KEY!
+)
