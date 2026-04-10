3bce0aee0adb7d460088097652586493e110c0ce
gjasmeen <jasmeen.garcha@edu.sait.ca>
2026-04-10 04:22:06 -0600

    edit

--- app/api/food/route.ts
+++ app/api/food/route.ts
@@ -1,5 +1,5 @@
 import { NextRequest, NextResponse } from "next/server";
-import { searchFoo, Food } from "@/lib/api/food/food";
+import { searchFood, Food } from "@/lib/api/food/food";
 import { createClient } from "@supabase/supabase-js";
 import { User } from "@supabase/supabase-js";
 
